2c7afcc9d9a4b4144b1f13d16ce7063e83f76985
Huthaifa Salman <H.j.Salman@students.ptuk.edu.ps>
2022-10-10 13:14:40 +0300

    deleted @storybook/testing-library

--- src/views/products/Products.stories.tsx
+++ src/views/products/Products.stories.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { within, userEvent } from "@storybook/testing-library";
 import { Products } from "./Products";
 
 export default {
@@ -21,8 +20,4 @@ export const LoggedOut = Template.bind({});
 export const LoggedIn = Template.bind({});
 
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
-LoggedIn.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  const loginButton = await canvas.getByRole("button", { name: /Log in/i });
-  await userEvent.click(loginButton);
-};
+LoggedIn.play = async ({ canvasElement }) => {};
