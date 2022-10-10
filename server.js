ba0c41c0e5628d49a8f4199783238052fb179546
Huthaifa Salman <H.j.Salman@students.ptuk.edu.ps>
2022-10-10 10:46:14 +0300

    modified server

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 // eslint-disable-next-line 
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./data/db.json");
-// const middlewares = jsonServer.defaults({
-//     static: "./build",
-// });
+const router = jsonServer.router("data/db.json");
+const middlewares = jsonServer.defaults({
+    static: "./build",
+});
 const PORT = process.env.PORT || 3001;
-// server.use(middlewares);
-// server.use(
-//     jsonServer.rewriter({
-//         "/api/*": "/$1",
-//     })
-// );
+server.use(middlewares);
+server.use(
+    jsonServer.rewriter({
+        "/api/*": "/$1",
+    })
+);
 server.use(router);
 server.listen(PORT, () => {
     console.log("Server is running");
