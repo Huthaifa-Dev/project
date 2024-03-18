3e4f6acfb0ddf4af87c403706cba38cc7147a77e
Huthaifa Salman <hsalman@restaurant365.com>
2024-03-18 14:35:58 +0200

    #66 - anything

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 const useInput = (validate: (value: string) => boolean) => {
-  const [value, setValue] = useState("");
+  const [inoutValue, setInputValue] = useState("");
   const [isTouched, setIsTouched] = useState(false);
 
-  const isValid = validate(value);
+  const isValid = validate(inoutValue);
   const hasError = !isValid && isTouched;
 
   const valueChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
+    setInputValue(event.target.value);
   };
 
   const inputBlurHandler = () => {
@@ -16,7 +16,7 @@ const useInput = (validate: (value: string) => boolean) => {
   };
 
   const reset = () => {
-    setValue("");
+    setInputValue("");
     setIsTouched(false);
   };
 
@@ -27,7 +27,7 @@ const useInput = (validate: (value: string) => boolean) => {
     reset: () => void;
     hasError: boolean;
   } = {
-    value,
+    value: inoutValue,
     onChange: valueChangeHandler,
     onBlur: inputBlurHandler,
     reset,
